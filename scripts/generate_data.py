fcf8c7e67749bb1060a3b844a02b05e16f11a5ad
Garrett Bellomy <dgbellomy@gmail.com>
2024-03-22 05:08:18 -0500

    Update generate_data.py

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,3 +1,15 @@
 #/usr/bin/env python3
 
-print('Hello, World!')
+import psycopg2
+import os
+
+DB_NAME="ogre_test"
+DB_USER="postgres"
+DB_HOST="psql-1.c1so8qiqiw95.us-east-2.rds.amazonaws.com"
+DB_PASSWD=os.environ['DB_PASSWD']
+
+conn = psycopg2.connect("dbname=%s user=%s password=%s host=%s" % (DB_NAME, DB_USER, DB_PASSWD, DB_HOST))
+#cur = conn.cursor()
+#cur.execute("select * from Characters")
+#records = cur.fetchall()
+#print(records)
